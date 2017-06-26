c96ec92fc829f47564ba8d693c7b633204553ec4
Nikolay Kagala <nibelungc@gmail.com>
2017-06-26 19:09:54 +0300

    Add section header in test project

--- ios-ScrollBar/ViewController.swift
+++ ios-ScrollBar/ViewController.swift
@@ -8,23 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, ScrollBarDataSource {
+class ViewController: UIViewController, UITableViewDataSource, ScrollBarDataSource, UITableViewDelegate {
     
     var tableView: UITableView!
     var scrollBar: ScrollBar!
-    var items = [String]()
+    var items = [[String]]()
     let identifier = "Cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        items = (0...1000).map { "Cell \($0)" }
+        items = [
+            (0...10).map { "Cell \($0)" },
+            (0...10).map { "Cell \($0)" },
+            (0...10).map { "Cell \($0)" }
+        ]
         
         tableView = UITableView(frame: view.bounds, style: .plain)
         tableView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         view.addSubview(tableView)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
         tableView.dataSource = self
+        tableView.delegate = self
+        tableView.sectionHeaderHeight = 20
         
         scrollBar = ScrollBar(scrollView: tableView)
         scrollBar.dataSource = self
@@ -34,19 +40,31 @@ class ViewController: UIViewController, UITableViewDataSource, ScrollBarDataSour
     
     func textForHintView(_ hintView: UIView, at point: CGPoint, for scrollBar: ScrollBar) -> String {
         guard let indexPath = tableView.indexPathForRow(at: point) else { return "" }
-        let title = items[indexPath.row]
+        let title = items[indexPath.section][indexPath.row]
         return title
     }
 
     // MARK: - UITableViewDataSource
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = items[indexPath.section][indexPath.row]
         return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .groupTableViewBackground
+        return view
     }
 }
